ba08312edf944a9eedafcf801194e87a708267ac
albraa hesham <akame.pato@gmail.com>
2018-03-03 15:53:35 +0200

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs= require('fs');
 const hbs = require('hbs');
+const port = process.env.PORT || 3000;
 var app = express();
 
 
@@ -58,6 +59,6 @@ app.get('/bad',(req,res) => {
   });
 });
 
-app.listen(3000, ()=>{
-  console.log('server is up in port 3000');
+app.listen(port, ()=>{
+  console.log(`server is up in port ${port}`);
 });
